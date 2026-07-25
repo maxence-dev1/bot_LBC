5880d5a845b97ef9e41e6832df3ea822c6eec7d0
maxence222 <m.julienne33670@gmail.com>
2026-07-25 10:49:11 +0200

    création du scraper des prix moyen du marché des composants

--- scraper_prix.py
+++ scraper_prix.py
@@ -4,28 +4,112 @@
     # Prend toutes les annonces de la recherche puis calcule la moyenne des prix (en enlevant les prix absurde)
     # Sauvegarde le fichier json actualisé avec les prix de la journée
 
+# Changement de plan : 
+#   Cause : Lors d'une recherche (RTX 4090) par exemple, des annonces de PC sortent aussi. Pour isoler uniquement celles de cartes graphiques, c'est difficile
+#   Solution : Récupérer tous les prix et description des toutes les annonces, envoyer les annonces à Groq, il me renvoie un Json de booléen -> calcul de la moyenne des prix valides
+
 import re
-
+from groq import Groq
 import requests
-
 from annonce import Annonce
-
+import json
 from dotenv import load_dotenv
 import os
 import scraper_annonce
 import scraper
 import json_fun
+import math
 
 load_dotenv()
 
 prix_composants = json_fun.read_json("prix_composants.json")
 
 
+load_dotenv()
 
-def get_mean_comp(comp):
-    # Problème : sur LBC quand tu cherches un composant, tu tombes aussi sur les PC qui le contienne. Solution ajouter des filtres. Sinon, vérifier dans la description si d'autres composants sont mentionnés
+
+
+
+def get_list_comp(comp):
+    """Récupère la liste des annonces des composants 'comp' et renvoie un string de la forme 'id Prix Body'"""
     list_comp = scraper.search_ads(comp)
-    for c in list_comp:
-        print(c["price"])
+    return list_comp
+    
 
-get_mean_comp(prix_composants["gpu"][0])
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+client = Groq(api_key=os.environ["GROQ_API_KEY"])
+INSTRUCTIONS ="""Tu reçois une liste d'annonces Leboncoin avec leur ID et leur prix. Pour chaque annonce, détermine si elle concerne UNIQUEMENT le composant seul (pas un PC complet, pas plusieurs composants vendus ensemble).
+
+Réponds UNIQUEMENT en JSON, sans texte autour, sous cette forme :
+{
+  "resultats": [
+    {"id": <id annonce>, "composant_seul": true},
+    {"id": <id annonce>, "composant_seul": false}
+  ]
+}
+"""
+
+
+
+
+def get_mean_price_comp(comp) -> dict:
+    """Renvoie la moyenne du prix d'un composant"""
+
+    annonces = get_list_comp(comp) # Récupère la liste des description des annonces
+
+    # Créé le texte qui sera envoyé à Groq au format 'list_id Prix Description'
+    text_annonces = ""
+    for c in annonces:
+            text_annonces = text_annonces + (f"\n\n [{c["list_id"]}] Prix : {c["price"][0]}€ - {scraper.ad_body(c["list_id"])}")
+
+
+    
+    # Partie IA
+    completion = client.chat.completions.create(
+        model="llama-3.3-70b-versatile",
+        messages=[
+            {"role": "system", "content": INSTRUCTIONS},
+            {"role": "user", "content": text_annonces}
+        ],
+        response_format={"type": "json_object"},
+        temperature=0,
+    )
+
+
+    result = json.loads(completion.choices[0].message.content)["resultats"] # Récupération du resultat
+    id_valides = {r["id"] for r in result if r["composant_seul"]} # Les ID qui ont été marqué comme valide
+
+    #print([scraper.ad_body(a["list_id"]) for a in annonces if a["list_id"] in id_valides])
+
+    price_list = [a["price"][0] for a in annonces if a["list_id"] in id_valides]
+
+
+    return sum(price_list)/len(price_list)
+
+
+
+for i in range(10):
+    print(get_mean_price_comp("RTX 4090"))
+
